32d554705efb6eb8200db206e768e819b4b9a7c2
Logan Miller <millerl1960@gmail.com>
2024-06-18 16:14:26 -0700

    Adds loading indicator

--- src/lib/components/GridView.svelte
+++ src/lib/components/GridView.svelte
@@ -4,151 +4,171 @@
 	import DownloadButton from './DownloadButton.svelte';
 	import type { FileItem } from '$lib/types';
 	import { selectedFilesStore } from '$lib/stores/selectedFilesStore';
-  
+	import { writable } from 'svelte/store';
+
 	export let currentSelectedFolder = '';
 
-	
 	let files: FileItem[] = [];
 	let selectedFile: FileItem | null = null;
 	let selectedFileIndex: number = 0;
 	let checkedFiles: Record<string, boolean> = {};
-  
+	let loading = writable(false); // Loading state
+
 	$: selectedFiles = $selectedFilesStore;
-  
+
 	async function fetchFiles() {
-	  const response = await fetch(`/api/files?path=${encodeURIComponent(currentSelectedFolder)}`);
-	  files = await response.json();
-	  checkedFiles = {};
-	  files.forEach((file) => {
-		checkedFiles[file.path] = selectedFiles.includes(file.path);
-	  });
+		loading.set(true); // Set loading to true before fetching files
+		const response = await fetch(`/api/files?path=${encodeURIComponent(currentSelectedFolder)}`);
+		files = await response.json();
+		checkedFiles = {};
+		files.forEach((file) => {
+			checkedFiles[file.path] = selectedFiles.includes(file.path);
+		});
+		loading.set(false); // Set loading to false after fetching files
 	}
-  
+
 	function openPreview(file: FileItem) {
-	  selectedFile = file;
-	  selectedFileIndex = files.findIndex((f) => f.path === file.path);
+		selectedFile = file;
+		selectedFileIndex = files.findIndex((f) => f.path === file.path);
 	}
-  
+
 	function closePreview() {
-	  selectedFile = null;
+		selectedFile = null;
 	}
-  
+
 	function toggleFileSelection(event: Event, filePath?: string) {
-	  if (!filePath) return;
-	  event.stopPropagation();
-	  checkedFiles[filePath] = !checkedFiles[filePath];
-	  if (checkedFiles[filePath]) {
-		selectedFilesStore.add(filePath);
-	  } else {
-		selectedFilesStore.remove(filePath);
-	  }
+		if (!filePath) return;
+		event.stopPropagation();
+		checkedFiles[filePath] = !checkedFiles[filePath];
+		if (checkedFiles[filePath]) {
+			selectedFilesStore.add(filePath);
+		} else {
+			selectedFilesStore.remove(filePath);
+		}
 	}
-  
+
 	function goToPreviousFile() {
-	  if (selectedFileIndex > 0) {
-		selectedFileIndex--;
-		selectedFile = files[selectedFileIndex];
-	  }
+		if (selectedFileIndex > 0) {
+			selectedFileIndex--;
+			selectedFile = files[selectedFileIndex];
+		}
 	}
-  
+
 	function goToNextFile() {
-	  if (selectedFileIndex < files.length - 1) {
-		selectedFileIndex++;
-		selectedFile = files[selectedFileIndex];
-	  }
+		if (selectedFileIndex < files.length - 1) {
+			selectedFileIndex++;
+			selectedFile = files[selectedFileIndex];
+		}
 	}
-  
+
 	$: {
-	  if (currentSelectedFolder) {
-		fetchFiles();
-	  }
+		if (currentSelectedFolder) {
+			fetchFiles();
+		}
 	}
-  </script>
-  
-  <div class="">
-	<div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-0">
-	  {#each files as file (file.path)}
+</script>
+
+<div class="relative">
+	{#if $loading}
+		<div class="flex items-center justify-center h-full">
+			<svg
+				class="animate-spin h-8 w-8 text-white"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"
+				></circle>
+				<path
+					class="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				></path>
+			</svg>
+		</div>
+	{:else}
+		<div class="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-0">
+			{#each files as file (file.path)}
+				<div
+					class="bg-xgray relative cursor-pointer group"
+					on:click|stopPropagation={() => openPreview(file)}
+				>
+					<div class="absolute top-0 left-0 p-2 z-10">
+						<input
+							type="checkbox"
+							class="form-checkbox h-5 w-5 accent-xteal image-checkbox"
+							bind:checked={checkedFiles[file.path]}
+							on:click|stopPropagation={(event) => toggleFileSelection(event, file.path)}
+						/>
+					</div>
+					{#if file.type === 'image'}
+						<img src={file.previewUrl} alt={file.name} class="w-full h-48 object-cover" />
+					{:else if file.type === 'video'}
+						<video src={file.fullUrl} class="w-full h-48 object-cover" />
+					{/if}
+					<div
+						class="absolute bottom-0 left-0 right-0 px-2 py-1 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+					>
+						<p class="text-sm truncate text-white">{file.name}</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+	{/if}
+
+	{#if selectedFile}
 		<div
-		  class="bg-xgray relative cursor-pointer group"
-		  on:click|stopPropagation={() => openPreview(file)}
-		>
-		  <div class="absolute top-0 left-0 p-2 z-10">
-			<input
-			  type="checkbox"
-			  class="form-checkbox h-5 w-5 accent-xteal image-checkbox"
-			  bind:checked={checkedFiles[file.path]}
-			  on:click|stopPropagation={(event) => toggleFileSelection(event, file.path)}
-			/>
-		  </div>
-		  {#if file.type === 'image'}
-			<img src={file.previewUrl} alt={file.name} class="w-full h-48 object-cover" />
-		  {:else if file.type === 'video'}
-			<video src={file.fullUrl} class="w-full h-48 object-cover" />
-		  {/if}
-		  <div
-			class="absolute bottom-0 left-0 right-0 px-2 py-1 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-		  >
-			<p class="text-sm truncate text-white">{file.name}</p>
-		  </div>
-		</div>
-	  {/each}
-	</div>
-  </div>
-  {#if selectedFile}
-	<div
-	  class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center z-50"
-	  on:click={closePreview}
-	>
-	  <div class="relative w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto" on:click|stopPropagation>
-		<div class="relative">
-		  {#if selectedFile.type === 'image'}
-			<img
-			  src={selectedFile.fullUrl}
-			  alt={selectedFile.name}
-			  class="w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto rounded-md"
-			/>
-		  {:else if selectedFile.type === 'video'}
-			<video
-			  src={selectedFile.fullUrl}
-			  class="w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto rounded-md"
-			  controls
-			/>
-		  {/if}
-		  <div class="absolute top-0 left-0 p-2">
-			<input
-			  type="checkbox"
-			  class="form-checkbox h-8 w-8 accent-xteal rounded-full cursor-pointer"
-			  bind:checked={checkedFiles[selectedFile.path]}
-			  on:click|stopPropagation={(event) => toggleFileSelection(event, selectedFile?.path)}
-			/>
-		  </div>
-		  <button
-			class="absolute top-0 right-1 m-2 text-white text-3xl font-bold focus:outline-none"
+			class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center z-50"
 			on:click={closePreview}
-		  >
-			×
-		  </button>
-		  {#if selectedFileIndex > 0}
-			<button
-			  class="fixed left-1 md:left-5 top-1/2 transform -translate-y-1/2 text-white text-4xl md:text-7xl font-bold focus:outline-none hover:text-xteal"
-			  on:click={goToPreviousFile}
-			>
-			  &lsaquo;
-			</button>
-		  {/if}
-		  {#if selectedFileIndex < files.length - 1}
-			<button
-			  class="fixed right-1 md:right-5 top-1/2 transform -translate-y-1/2 text-white text-4xl md:text-7xl font-bold focus:outline-none hover:text-xteal"
-			  on:click={goToNextFile}
-			>
-			  &rsaquo;
-			</button>
-		  {/if}
-		  <div class="absolute bottom-[-35px] md:bottom-1 left-0 right-0 flex justify-center">
-			<DownloadButton {currentSelectedFolder} filePath={selectedFile.path} />
-		  </div>
+		>
+			<div class="relative w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto" on:click|stopPropagation>
+				<div class="relative">
+					{#if selectedFile.type === 'image'}
+						<img
+							src={selectedFile.fullUrl}
+							alt={selectedFile.name}
+							class="w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto rounded-md"
+						/>
+					{:else if selectedFile.type === 'video'}
+						<video
+							src={selectedFile.fullUrl}
+							class="w-auto h-auto max-h-[90vh] max-w-[90vw] m-auto rounded-md"
+							controls
+						/>
+					{/if}
+					<div class="absolute top-0 left-0 p-2">
+						<input
+							type="checkbox"
+							class="form-checkbox h-8 w-8 accent-xteal rounded-full cursor-pointer"
+							bind:checked={checkedFiles[selectedFile.path]}
+							on:click|stopPropagation={(event) => toggleFileSelection(event, selectedFile?.path)}
+						/>
+					</div>
+					<button
+						class="absolute top-0 right-1 m-2 text-white text-3xl font-bold focus:outline-none"
+						on:click={closePreview}>×</button
+					>
+					{#if selectedFileIndex > 0}
+						<button
+							class="fixed left-1 md:left-5 top-1/2 transform -translate-y-1/2 text-white text-4xl md:text-7xl font-bold focus:outline-none hover:text-xteal"
+							on:click={goToPreviousFile}
+						>
+							&lsaquo;
+						</button>
+					{/if}
+					{#if selectedFileIndex < files.length - 1}
+						<button
+							class="fixed right-1 md:right-5 top-1/2 transform -translate-y-1/2 text-white text-4xl md:text-7xl font-bold focus:outline-none hover:text-xteal"
+							on:click={goToNextFile}
+						>
+							&rsaquo;
+						</button>
+					{/if}
+					<div class="absolute bottom-[-35px] md:bottom-1 left-0 right-0 flex justify-center">
+						<DownloadButton {currentSelectedFolder} filePath={selectedFile.path} />
+					</div>
+				</div>
+			</div>
 		</div>
-	  </div>
-	</div>
-  {/if}
-  
+	{/if}
+</div>
