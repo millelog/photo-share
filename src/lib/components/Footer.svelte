8534836474101e045036bb0f7f07bf65a81e36d4
Logan Miller <millerl1960@gmail.com>
2024-06-18 22:06:14 -0700

    Removes review button

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,14 +1,11 @@
 <!--src/lib/components/Footer.svelte-->
 <footer class="bg-xgray text-white p-2 flex flex-col items-center md:flex-row md:justify-between px-8">
     <div class="text-center md:text-left mb-4 md:mb-0">
-      <p class="text-lg font-bold text-balance">Like this website? Check out our business and leave us a review!</p>
+      <p class="text-lg font-bold text-balance">Like this website? Check out our web design business!</p>
     </div>
     <div class="flex space-x-2 xl:space-x-4">
       <a href="https://www.cascadeonlinedesign.com" target="_blank" class="bg-xteal px-2 xl:px-4 py-2 text-xgray font-bold rounded-md hover:bg-xdteal transition text-center">
         Visit Our Website
-      </a>
-      <a href="https://g.page/r/CbhecdRz3zVJEAI/review" target="_blank" class="bg-xteal px-2 xl:px-4 py-2 text-xgray font-bold rounded-md hover:bg-xdteal transition text-center">
-        Leave a Review
       </a>
     </div>
     <a href="https://www.cascadeonlinedesign.com" target="_blank" class="md:hidden">
