65aa7d4cd7bb580ed9993179dd7f976444f8c272
Logan Miller <millerl1960@gmail.com>
2024-06-19 20:59:39 -0700

    Revert "Changes default path"
    This reverts commit a182fc9f42721020b34bf6316c752c2b6bde15cf.

--- src/routes/wedding/photos/+page.svelte
+++ src/routes/wedding/photos/+page.svelte
@@ -6,8 +6,7 @@
 
   // Dynamically construct the selected folder based on the URL path
   onMount(() => {
-    var pathSegments = $page.url.pathname.split('/').filter(Boolean);
-    pathSegments = ['wedding', 'printable photos'];
+    const pathSegments = $page.url.pathname.split('/').filter(Boolean);
     const separator = process.env.NODE_ENV === 'production' ? '/' : '\\';
     selectedFolder.set(pathSegments.join(separator));
   });
