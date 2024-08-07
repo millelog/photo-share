e6927071840039bd2a288fb733ae83bf730700ef
Logan Miller <millerl1960@gmail.com>
2024-08-06 20:19:12 -0700

    Adds share path

---
+++ src/routes/share/+page.svelte
@@ -0,0 +1,13 @@
+<!--routes/wedding/videos/+page.svelte-->
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+    import { selectedFolder } from '$lib/stores/selectedFolderStore';
+  
+    // Dynamically construct the selected folder based on the URL path
+    onMount(() => {
+      const pathSegments = $page.url.pathname.split('/').filter(Boolean);
+      const separator = process.env.NODE_ENV === 'production' ? '/' : '\\';
+      selectedFolder.set(pathSegments.join(separator));
+    });
+  </script>
